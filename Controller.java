b572f309d420bf59adf7794064150ceec14f3ef8
rbem <debemrafael@gmail.com>
2019-06-29 22:52:59 -0300

    wip

--- Controller.java
+++ Controller.java
@@ -14,20 +14,19 @@ public class Controller{
     private boolean ulaFonteA;
     private boolean escReg;
     private boolean regDest;
+    private Ula ula;
     int regA;
     int regB;
     int aluOut;
     
-
     public Controller(String inst, int pc){
+        ula = new Ula();
         this.inst = inst;
         run(pc);
-        
     }
 
-
-
     public void run(int pc){
+        //ETAPA 2
         System.out.println("---------------------------------");
         System.out.println("Etapa 2: ");
         regA = Integer.parseInt(inst.substring(6,11),2);
@@ -35,42 +34,84 @@ public class Controller{
         aluOut = Integer.parseInt(inst.substring(6),2);
         String operation = decode(inst);
         
-        System.out.println("RegA = "+regA+" | RegB = "+regB+" | ALUOut = "+aluOut+" | Operation = "+operation);
+        System.out.println("RegA = " + regA + " | RegB = " + regB + " | ALUOut = " + aluOut + " | Operation = " + operation);
         
+        //ETAPA 3
+        String tipo = getTipo(operation);
+        switch(tipo){
+            case "tipo_r":
+                
 
 
+
+                break;
+            case "tipo_i":
+                break;
+            case "jump":
+                break;
+            case "store":
+                break;
+            case "load":
+                break;
+            case "branch":
+                break;
+            default:
+                break;
+        }
     }
 
-
-    public  String decode(String instruction) {
+    public String decode(String instruction) {
         String opcode = instruction.substring(0, 6);
         switch (opcode) {
-        case "001111":
-            return "lui";
-        case "001101":
-            return "ori";
-        case "000000":
-            String funct = instruction.substring(26);
-            if (funct.equals("100001"))
-                return "addu";
-        case " ":
-            return " ";
-        case "  ":
-            return "  ";
-        case "   ":
-            return "   ";
+            case "001111":
+                return "lui";
+            case "001101":
+                return "ori";
+            case "001000":
+                return "addiu";
+            case "000000":
+                String funct = instruction.substring(26);
+                if (funct.equals("100001"))
+                    return "addu";
+                else if(funct.equals("101011"))
+                    return "slt";
+                else if(funct.equals("100100"))
+                    return "and";
+                else if(funct.equals("000000"))
+                    return "sll";
+                else if(funct.equals("000010"))
+                    return "srl";
+            case "000010":
+                return "j";
+            case "000100":
+                return "beq";
+            case "101011":
+                return "sw";
+            case "100011":
+                return "lw";
         }
         return "";
     }
 
     public String getTipo(String op){
         switch (op) {
-            case "":
-                
-                break;
-        
+            case "lw":
+                return "load";
+            case "sw":
+                return "store";
+            case "beq":
+                return "branch";
+            case "j":
+                return "jump";
+            case "addiu":
+            case "lui":
+            case "ori":
+                return "tipo_i";
+            case "sll":
+            case "srl":
+                return "tipo_r_shamt";
             default:
-                break;
+                return "tipo_r";
         }
     }
     
---
+++ Ula.java
@@ -0,0 +1,55 @@
+// This unit implements the MIPS ULA
+// lui, ori, addu, addiu, lw, sw, beq, slt, j, and, sll, srl
+
+// Inputs: ULAOp, ULASrcA, ULASrcB 
+// ULAOp:
+//  0: ULA performs an addition
+//  1: ULA performs a subtraction
+//  2: ULA performs a comparison (beq)
+
+public class Ula{
+    private int srcA;
+    private int srcB;
+    private String op;
+
+    public Ula(){
+        this.srcA = 0;
+        this.srcB = 0;
+        this.op = "";
+    }
+
+    public void setSrcA(int srcA){
+        this.srcA = srcA;
+    }
+
+    public void setSrcB(int srcB){
+        this.srcB = srcB;
+    }
+    
+    public void setULAOp(String op){
+        this.op = op;
+    }
+
+    public String tipoR(int srcA, int srcB, String op){
+        switch(op){
+            case "addu":
+                return String.valueOf(srcA + srcB);
+            case "slt":
+                return (srcA < srcB) ? "1" : "0";
+            case "and":
+                return String.valueOf(srcA & srcB);
+            }
+        return "";
+    }
+
+    public String tipoRShamt(int srcA, String op, int shamt){
+        switch(op){
+            case "sll":
+                return String.valueOf(srcA << shamt);
+            case "srl":
+                return String.valueOf(srcA >> shamt);
+        }
+        return "";
+    }
+
+}
