b5163e486d3149ef07eb3c8d3b8357832cf74e47
Pedro Chem <pedrochem@hotmail.com>
2019-07-02 21:43:37 -0300

    outramensagem

--- Teste.java
+++
@@ -1,9 +0,0 @@
-public class Teste{
-    public static void main(String[] args) {
-        String line = "label: 1000110101";
-        String line2 = "1000110101";
-
-        String[] campos = line2.split(":");
-        System.out.println(campos[0]);
-    } 
-}
